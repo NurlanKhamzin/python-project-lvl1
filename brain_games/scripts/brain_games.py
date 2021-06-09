29048d3fa39ad4321f62fe0357cb181c03c566ff
Nurlan Khamzin <khamzin1991@gmail.com>
2021-06-10 01:58:12 +0500

    fifth part

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-from brain_games.cli import welcome_user
+from . import brain_even
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    welcome_user()
+    brain_even.main()
 
 
 if __name__ == '__main__':
