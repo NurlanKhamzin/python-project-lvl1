ab1111ca1fe11abf12156887df9461ce21fbf2ed
Nurlan Khamzin <khamzin1991@gmail.com>
2021-07-11 19:52:30 +0500

    fixing engine after most comments

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -14,7 +14,7 @@ def run(game):
         if user_answer == correct_answer:
             print("Correct!")
         else:
-            print(f"{user_answer}is wrong answer ;(. "
+            print(f"{user_answer} is wrong answer ;(. "
                   f"Correct answer was {correct_answer}.")
             print(f"Let's try again, {name}!")
             break
