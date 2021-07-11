b1124399a97cf05fdf8cefe1d4f2b719ad3ab982
Nurlan Khamzin <khamzin1991@gmail.com>
2021-07-11 22:20:55 +0500

    fixing progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,17 +3,21 @@ from random import randint, randrange
 GAME_RULE = "What number is missing in the progression?"
 
 
-def generate_question_answer():
-    task = []
-    correct = []
+def generate_progression():
+    progression = []
+    checker = []
     a1 = randint(1, 10)
     d = randint(1, 10)
     for i in range(10):
         formula = a1 + (i - 1) * d
-        task.append(formula)
-        correct.append(formula)
-    random = randrange(len(task))
-    task[random] = '..'
-    task = ' '.join([str(n) for n in task])
-    correct_answer = str(correct[random])
+        progression.append(formula)
+        checker.append(formula)
+    random = randrange(len(progression))
+    progression[random] = '..'
+    progression = ' '.join([str(n) for n in progression])
+    return progression, str(checker[random])
+
+
+def generate_question_answer():
+    task, correct_answer = generate_progression()
     return task, correct_answer
